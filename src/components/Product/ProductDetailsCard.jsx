d8c3a56066939a95383646552bf51b4674f6ef59
MaaX59 <Maax.strandberg@gmail.com>
2023-07-04 14:39:15 +0200

    productCardDetails page frontend finished

--- src/components/Product/ProductDetailsCard.jsx
+++ src/components/Product/ProductDetailsCard.jsx
@@ -1,13 +1,25 @@
 import React, { useState } from "react";
 import { RxCross1 } from "react-icons/rx";
 import Ratings from "./Ratings";
-
-
+import {
+  AiFillHeart,
+  AiOutlineHeart,
+  AiOutlineShoppingCart,
+} from "react-icons/ai";
 
 function ProductDetailsCard({ setOpen, product }) {
   const [count, setCount] = useState(1);
   const [click, setClick] = useState(false);
   const [select, setSelect] = useState(false);
+
+  const countDown = () => {
+    if (count > 1) {
+      setCount(count - 1);
+    }
+  };
+  const countUp = () => {
+    setCount(count + 1);
+  };
 
   return (
     <div className="bg-[#fff]">
@@ -27,15 +39,78 @@ function ProductDetailsCard({ setOpen, product }) {
                   alt={product.name}
                 />
                 <div className="flex">
-                    <img src={product.sellerAvatar} alt={product.seller} className="w-[50px] h-[50px] rounded-full mr-2"/>
+                  <img
+                    src={product.sellerAvatar}
+                    alt={product.seller}
+                    className="w-[50px] h-[50px] rounded-full mr-2"
+                  />
                 </div>
-                <div className="flex items-center"> <h3 className="pt-3 text-[15px] text-blue-400 pb-3 flex"> {product.seller}
+                <div className="flex items-center">
+                  {" "}
+                  <h3 className="pt-3 text-[15px] text-blue-400 pb-3 flex">
+                    {" "}
+                    {product.seller}
                   </h3>
-                  <h5 className="flex flex-row" > <Ratings num={product.ratings  } /></h5>
-                  
+                  <h5 className="flex flex-row">
+                    {" "}
+                    <Ratings num={product.ratings} />
+                  </h5>
                 </div>
               </div>
-            </div>
+              <div className="flex flex-col w-full 800px:w-[50%] pt-5 pl-[5px] pr-[5px]">
+                <h1 className="flex text-[30px] font-[600] font-Roboto text-[#333]">
+                  {product.name}
+                </h1>
+                <p>{product.description}</p>
+                <div className="flex pt-3">
+                  <h3 className="flex font-bold text-[18px] text-[#333] font-Roboto">
+                    {product.price + "$"}{" "}
+                  </h3>
+                </div>
+                <div className="flex">
+                  <button
+                    className="bg-gradient-to-t from-teal-400 to-teal-500 text-white font-bold rounded-l px-4 py-2 shadow-lg hover:opacity-75 transition duration-300 ease-in-out "
+                    onClick={countDown}
+                  >
+                    -
+                  </button>
+                  <span className="bg-gray-200 text-gray-800 font-medium px-4 py-[11px]">
+                    {count}
+                  </span>
+                  <button
+                    className="bg-gradient-to-t from-teal-400 to-teal-500 text-white font-bold rounded-r px-4 py-2 shadow-lg hover:opacity-75 transition duration-300 ease-in-out "
+                    onClick={countUp}
+                  >
+                    +
+                  </button>
+                  {click ? (
+                    <AiFillHeart
+                      size={22}
+                      className="cursor-pointer "
+                      onClick={() => setClick(!click)}
+                      color={click ? "red" : "black"}
+                      title="Remove from wishlist"
+                    />
+                  ) : (
+                    <AiOutlineHeart
+                      size={22}
+                      className="cursor-pointer"
+                      onClick={() => setClick(!click)}
+                      color={click ? "red" : "black"}
+                      title="Add to wishlist"
+                    />
+                  )}
+                </div><div className="w-[150px] bg-black my-3 mt-6 h-11 flex items-center justify-center rounded-[4px]">
+                <span className="text-white flex items-center">
+                  Add to cart <AiOutlineShoppingCart className="ml-l" />
+                </span>
+              </div>
+              </div>
+              <div className="flex items-center mt-12 justify-between pr-3"></div>
+
+              
+            </div> 
+            
           </div>
         </div>
       ) : null}
--- src/components/Product/Ratings.jsx
+++ src/components/Product/Ratings.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function Ratings(num) {
-    console.log(num.num)
+  
 function getStarRating(num) {
 
         const roundedNumber = Math.round(num.num);
