732f3fa37b097abf98c754615507f6b1d0c69507
MaaX59 <Maax.strandberg@gmail.com>
2023-07-25 20:58:54 +0200

    adad

--- src/api/serviceSignUpImage.js
+++ src/api/serviceSignUpImage.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { server } from "../../server";
+import { server } from "../server";
  
 const api = axios.create({
   // make sure you use PORT = 5005 (the port where our server is running)
